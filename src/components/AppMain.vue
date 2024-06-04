e9a8ad7af50b7c22e7e88df24e9236ce90d19f29
ClaPascarella <claudiopascarella1996@gmail.com>
2024-06-05 01:36:30 +0200

    card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -36,51 +36,161 @@
 
 
 
-  <div class="container m-0 mt-5 d-flex flex-row"">
+  <div class="container m-0 mt-5 d-flex flex-row">
     <div class=" row">
 
-    <div class="col-7" style="margin-top: 20rem;">
+      <div class="col-7" style="margin-top: 20rem;">
 
-      <div class="d-flex flex-row mt-5">
-        <hr class="hr1">
-        <hr class="hr2 ms-3">
+        <div class="d-flex flex-row mt-5">
+          <hr class="hr1">
+          <hr class="hr2 ms-3">
+        </div>
+        <h1 style="font-size: 3.5rem;">Our recent <g1 style="color: #00D9A6;">web designs</g1> &
+          some examples of
+          past <h1 style="color: #00D9A6; font-size: 3.5rem;">projects</h1>
+        </h1>
+
+        <div class="d-flex flex-row">
+          <hr class="hr1">
+          <hr class="hr2 ms-3">
+        </div>
       </div>
-      <h1 style="font-size: 3.5rem;">Our recent <g1 style="color: #00D9A6;">web designs</g1> &
-        some examples of
-        past <h1 style="color: #00D9A6; font-size: 3.5rem;">projects</h1>
-      </h1>
 
-      <div class="d-flex flex-row">
-        <hr class="hr1">
-        <hr class="hr2 ms-3">
+      <div class="col-5" style="margin-top: 30rem;">
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
+          texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+          ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
       </div>
-    </div>
-
-    <div class="col-5" style="margin-top: 30rem;">
-      <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-        texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
-        ocean.Separated they live in Bookmarksgrove.Far far away, behind the word mountains, far from the countries
-        Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
     </div>
   </div>
+
+  <div class="container mt-4">
+    <div class="row d-flex justify-content-center">
+      <div class="col-3">
+        <img src="../assets/images/ina-soulis-227104-unsplash-1024x1024.jpg" alt="" class="cardImg">
+      </div>
+      <div class="col-3">
+        <img src="../assets/images/sunisa-misa-531163-unsplash-1024x1024.jpg" alt="" class="cardImg">
+      </div>
+      <div class="col-3">
+        <img src="../assets/images/355H-1024x1024.jpg" alt="" class="cardImg">
+      </div>
+      <div class="col-3">
+        <img src="../assets/images/photo-1448932252197-d19750584e56-1024x1024.jpg" alt="" class="cardImg">
+      </div>
+    </div>
   </div>
 
   <div class="container">
-    <div class="row">
+    <div class="row mt-2 d-flex justify-content-center">
       <div class="col-3">
-        <img src="../assets/images/ina-soulis-227104-unsplash-1024x1024.jpg" alt="">
+        <img src="../assets/images/business-competition-PB366D8-1024x1024.jpg" alt="" class="cardImg">
       </div>
       <div class="col-3">
-        <img src="../assets/images/sunisa-misa-531163-unsplash-1024x1024.jpg" alt="">
+        <img src="../assets/images/cozy-sofa-in-living-room-PQR5AB9-1024x1024.jpg" alt="" class="cardImg">
       </div>
       <div class="col-3">
-        <img src="../assets/images/355H-1024x1024.jpg" alt="">
+        <img src="../assets/images/aa9a4539-PQGJ7HU-1024x1024.jpg" alt="" class="cardImg">
       </div>
       <div class="col-3">
-        <img src="../assets/images/photo-1448932252197-d19750584e56-1024x1024.jpg" alt="">
+        <img src="../assets/images/cody-davis-253928-unsplash-1024x1024.jpg" alt="" class="cardImg">
       </div>
     </div>
   </div>
+
+  <div class="mt-5 d-flex justify-content-center">
+    <button class="btn-green2 ">READ MORE</button>
+  </div>
+
+
+
+  <div class="container-fluis m-0 mt-5" style="background-color: #E5E3DF;">
+    <div class="row">
+      <div class="col-6">
+        <div class="cardGreen2 ms-5"></div>
+        <div class="imgPerson3 ms-5">
+          <div class="number-container">
+            <span class="number">0</span>
+          </div>
+        </div>
+
+        <div class="col-6">
+          <div class="containerWhite">
+            <h1 class="ms-5" style="font-size: 3rem;">We <span style="color: #00D9A6;">Love</span> What</h1>
+            <h1 class="ms-5" style="font-size: 3rem;">We Do</h1>
+
+            <div class="d-flex flex-row ms-5">
+              <hr class="hr1">
+              <hr class="hr2 ms-3">
+            </div>
+
+            <div class="ms-5">
+              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+                blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language
+                ocean.Separated they live in Bookmarksgrove. Far far away, behind the word mountains. large language
+                ocean. far from the countries Vokalia and Consonantia</p>
+            </div>
+
+            <div class="ms-5">
+              <ul style="list-style: none;">
+                <li class="mb-3"> <i class="fa-solid fa-check text-success"></i> far far away, behind the word mountains
+                </li>
+                <li class="mb-3"> <i class="fa-solid fa-check text-success"></i> large language ocean</li>
+                <li> <i class="fa-solid fa-check text-success"></i> far from the countries Vokalia and Consonantia</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5 m-0">
+    <div class="row">
+
+      <div class="col-6">
+        <h1 style="font-size: 4.5rem;">With <span style="color: #00D9A6;">Live Chat</span>
+          <span style="color: #00D9A6;"> 24/7</span> Support
+        </h1>
+
+        <div class="d-flex flex-row ">
+          <hr class="hr1">
+          <hr class="hr2 ms-3">
+        </div>
+
+        <div>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the
+            blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics
+          </p>
+        </div>
+      </div>
+
+      <div class="col-2 cardBlack p-0">
+
+        <div class="mt-3" style="width: 100%; height: auto; background-color: white;">
+          <div class="d-flex flex-column justify-content-center align-items-center">
+            <h2>DESIGNING</h2>
+            <p>All Designs</p>
+          </div>
+        </div>
+        <p class="text-white"><span style="font-size: 2.5rem;">$10</span> /Design</p>
+
+      </div>
+
+
+
+
+
+
+
+    </div>
+  </div>
+
+
+
+
+
 
 
 
@@ -158,6 +268,70 @@
 
 .btn-green:hover {
   background: linear-gradient(to right, gray, black);
+
+}
+
+.cardImg {
+  width: 20rem;
+  height: 20rem;
+  border-radius: 5px;
+  transition: transform 0.3s ease;
+}
+
+.cardImg:hover {
+  transform: scale(1.2);
+  opacity: 0.9;
+
+}
+
+.btn-green2 {
+  background: linear-gradient(to right, #64CE74, #21D8A6);
+  color: white;
+  font-size: 0.8rem;
+  border-radius: 25px;
+  padding: 20px 40px;
+  border: none;
+}
+
+.btn-green2:hover {
+  background: linear-gradient(to right, gray, black);
+
+}
+
+.cardGreen2 {
+  background: linear-gradient(to right, #64CE74, #21D8A6);
+  background-size: cover;
+  width: 800px;
+  height: 600px;
+  border-radius: 25px;
+  position: absolute;
+  margin-top: 10rem;
+}
+
+.imgPerson3 {
+  background-image: url(../assets/images/person-with-long-curly-hair-PZ99QM2-2x.jpg);
+  background-size: cover;
+  width: 800px;
+  height: 600px;
+  opacity: 0.1;
+  border-radius: 25px;
+  margin-top: 10rem;
+}
+
+.containerWhite {
+  background-color: white;
+  width: 850px;
+  height: 600px;
+  border-radius: 25px;
+  margin-left: 40rem;
+  margin-top: -33rem;
+  position: relative;
+  z-index: 6;
+}
+
+.cardBlack {
+  background: linear-gradient(to right, gray, black);
+  border-radius: 20px;
 
 }
 </style>
